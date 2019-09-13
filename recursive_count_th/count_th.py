cdd461eb7b8597d02f55168703b4ee76a9b53d78
christiansallen <christiansallen@gmail.com>
2019-09-14 00:40:12 +0800

    finished count and robot sort

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,6 +4,14 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 
+'''
+Check is word is less than 2 characters, if it is return 0
+delcare global index and count to increment
+base case is when index equals length of word - 1
+If word[index] is t and word[index+1] = h then increment count by 1
+use recursion to call this as many times before base case if reached
+'''
+
 index = 0
 count = 0
 
@@ -12,6 +20,9 @@ def count_th(word):
 
     global index
     global count
+
+    if(len(word) < 2):
+        return 0
 
     if(index == len(word)-1):
         return count
